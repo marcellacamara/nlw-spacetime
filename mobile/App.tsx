27a4ca9ffbefd9d42c7bcb95041b3ce2b6a4a1d0
marcella <marcella.camara@sagatechbrasil.com.br>
2023-06-07 19:13:22 -0300

    instalação de bibliotecas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, ImageBackground } from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import { styled } from "nativewind";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-5xl font-bold text-gray-50">Hello World</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <Text className="font-title text-5xl text-gray-50"></Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
